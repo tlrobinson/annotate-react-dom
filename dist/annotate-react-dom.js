f2763fcb457a022cf22d8341f75dde8d12830c15
Tom Robinson <tlrobinson@gmail.com>
2019-04-19 15:18:54 -0700

    support react 16

--- dist/annotate-react-dom.js
+++ dist/annotate-react-dom.js
@@ -1,7 +1,7 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 exports.annotateReactNode = annotateReactNode;
 exports.annotateReactNodes = annotateReactNodes;
@@ -9,134 +9,147 @@ exports.installAsynchronousAnnotator = installAsynchronousAnnotator;
 exports.installSynchronousAnnotator = installSynchronousAnnotator;
 exports.getReactInternalInstance = getReactInternalInstance;
 exports.getReactOwner = getReactOwner;
-exports.getReactElementName = getReactElementName;
+exports.getReactType = getReactType;
+exports.getReactTypeName = getReactTypeName;
 function annotateReactNode(node) {
-    var _ref = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
-        _ref$attribute = _ref.attribute,
-        attribute = _ref$attribute === undefined ? "react" : _ref$attribute;
+  var _ref = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
+      _ref$attribute = _ref.attribute,
+      attribute = _ref$attribute === undefined ? "react" : _ref$attribute;
 
-    if (node && node.nodeType === Node.ELEMENT_NODE) {
-        var element = node;
-        var owner = getReactOwner(element);
-        var parentOwner = getReactOwner(element.parentNode);
-        // only set it for the root element of any particular owner
-        if (owner && owner !== parentOwner) {
-            element.setAttribute(attribute, getReactElementName(owner._currentElement));
-        }
+  if (node && node.nodeType === Node.ELEMENT_NODE) {
+    var element = node;
+    var owner = getReactOwner(element);
+    var parentOwner = getReactOwner(element.parentNode);
+    // only set it for the root element of any particular owner
+    if (element && owner && owner !== parentOwner) {
+      var _type = getReactTypeName(owner);
+      if (_type) {
+        element.setAttribute(attribute, _type);
+      }
     }
+  }
 }
 
 // One-time functions to annotate a single node or entire tree
 
 function annotateReactNodes() {
-    var node = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : document;
-    var options = arguments[1];
+  var node = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : document;
+  var options = arguments[1];
 
-    annotateReactNode(node, options);
-    var current = node.firstChild;
-    while (current) {
-        annotateReactNodes(current, options);
-        current = current.nextSibling;
-    }
+  annotateReactNode(node, options);
+  var current = node.firstChild;
+  while (current) {
+    annotateReactNodes(current, options);
+    current = current.nextSibling;
+  }
 }
 
 // ANNOTATORS
 // Keep the annotations updated as new DOM nodes are inserted
 
 function installAsynchronousAnnotator() {
-    var node = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : document;
-    var options = arguments[1];
+  var node = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : document;
+  var options = arguments[1];
 
-    var observer = new MutationObserver(function (mutations) {
-        var _iteratorNormalCompletion = true;
-        var _didIteratorError = false;
-        var _iteratorError = undefined;
+  var observer = new MutationObserver(function (mutations) {
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      for (var _iterator = mutations[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var mutation = _step.value;
+        var _iteratorNormalCompletion2 = true;
+        var _didIteratorError2 = false;
+        var _iteratorError2 = undefined;
 
         try {
-            for (var _iterator = mutations[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-                var mutation = _step.value;
-                var _iteratorNormalCompletion2 = true;
-                var _didIteratorError2 = false;
-                var _iteratorError2 = undefined;
+          for (var _iterator2 = mutation.addedNodes[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+            var _node = _step2.value;
 
-                try {
-                    for (var _iterator2 = mutation.addedNodes[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-                        var _node = _step2.value;
-
-                        annotateReactNodes(_node, options);
-                    }
-                } catch (err) {
-                    _didIteratorError2 = true;
-                    _iteratorError2 = err;
-                } finally {
-                    try {
-                        if (!_iteratorNormalCompletion2 && _iterator2.return) {
-                            _iterator2.return();
-                        }
-                    } finally {
-                        if (_didIteratorError2) {
-                            throw _iteratorError2;
-                        }
-                    }
-                }
-            }
+            annotateReactNodes(_node, options);
+          }
         } catch (err) {
-            _didIteratorError = true;
-            _iteratorError = err;
+          _didIteratorError2 = true;
+          _iteratorError2 = err;
         } finally {
-            try {
-                if (!_iteratorNormalCompletion && _iterator.return) {
-                    _iterator.return();
-                }
-            } finally {
-                if (_didIteratorError) {
-                    throw _iteratorError;
-                }
+          try {
+            if (!_iteratorNormalCompletion2 && _iterator2.return) {
+              _iterator2.return();
             }
+          } finally {
+            if (_didIteratorError2) {
+              throw _iteratorError2;
+            }
+          }
         }
-    });
-    annotateReactNodes(node, options);
-    observer.observe(node, { childList: true, subtree: true });
-    return function () {
-        return observer.disconnect();
-    };
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+          _iterator.return();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
+  });
+  annotateReactNodes(node, options);
+  observer.observe(node, { childList: true, subtree: true });
+  return function () {
+    return observer.disconnect();
+  };
 }
 
 function installSynchronousAnnotator() {
-    var node = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : document;
-    var options = arguments[1];
+  var node = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : document;
+  var options = arguments[1];
 
-    var listener = function listener(e) {
-        annotateReactNodes(e.target, options);
-    };
-    annotateReactNodes(node, options);
-    node.addEventListener("DOMNodeInserted", listener, false);
-    return function () {
-        return node.removeEventListener("DOMNodeInserted", listener, false);
-    };
+  var listener = function listener(e) {
+    annotateReactNodes(e.target, options);
+  };
+  annotateReactNodes(node, options);
+  node.addEventListener("DOMNodeInserted", listener, false);
+  return function () {
+    return node.removeEventListener("DOMNodeInserted", listener, false);
+  };
 }
 
 // HELPERS
 
 function getReactInternalInstance(node) {
-    if (node) {
-        for (var _name in node) {
-            if (_name.startsWith("__reactInternalInstance$")) {
-                // $FlowFixMe
-                return node[_name];
-            }
-        }
+  if (node) {
+    for (var _name in node) {
+      if (_name.startsWith("__reactInternalInstance$")) {
+        // $FlowFixMe
+        return node[_name];
+      }
     }
+  }
 }
 
 function getReactOwner(node) {
-    var internalInstance = getReactInternalInstance(node);
-    if (internalInstance) {
-        return internalInstance._currentElement._owner;
-    }
+  var internalInstance = getReactInternalInstance(node);
+  if (internalInstance) {
+    return internalInstance._currentElement ? // pre-Fiber
+    internalInstance._currentElement._owner : // Fiber
+    internalInstance._debugOwner;
+  }
 }
 
 // works for both class and stateless function components
-function getReactElementName(element) {
-    return element.type.displayName || element.type.name;
+function getReactType(instance) {
+  return instance._currentElement ? // pre-Fiber
+  instance._currentElement.type : // Fiber
+  instance.type;
+}
+
+function getReactTypeName(instance) {
+  var type = getReactType(instance);
+  return !type ? null : typeof type === "string" ? type : type.displayName || type.name;
 }
--- src/annotate-react-dom.js
+++ src/annotate-react-dom.js
@@ -3,88 +3,126 @@
 // One-time functions to annotate a single node or entire tree
 
 type Options = {
-    attribute?: string,
+  attribute?: string
 };
 
-export function annotateReactNode(node: Node, { attribute = "react" }: Options = {}): void {
-    if (node && node.nodeType === Node.ELEMENT_NODE) {
-        let element: Element = (node: any);
-        let owner = getReactOwner(element);
-        let parentOwner = getReactOwner(element.parentNode);
-        // only set it for the root element of any particular owner
-        if (owner && owner !== parentOwner) {
-            element.setAttribute(attribute, getReactElementName(owner._currentElement))
-        }
+export function annotateReactNode(
+  node: Node,
+  { attribute = "react" }: Options = {}
+): void {
+  if (node && node.nodeType === Node.ELEMENT_NODE) {
+    const element: Element = (node: any);
+    const owner = getReactOwner(element);
+    const parentOwner = getReactOwner(element.parentNode);
+    // only set it for the root element of any particular owner
+    if (element && owner && owner !== parentOwner) {
+      const type = getReactTypeName(owner);
+      if (type) {
+        element.setAttribute(attribute, type);
+      }
     }
+  }
 }
 
-export function annotateReactNodes(node: Node = document, options: Options): void {
-    annotateReactNode(node, options);
-    let current = node.firstChild;
-    while (current) {
-        annotateReactNodes(current, options);
-        current = current.nextSibling;
-    }
+export function annotateReactNodes(
+  node: Node = document,
+  options: Options
+): void {
+  annotateReactNode(node, options);
+  let current = node.firstChild;
+  while (current) {
+    annotateReactNodes(current, options);
+    current = current.nextSibling;
+  }
 }
 
 // ANNOTATORS
 // Keep the annotations updated as new DOM nodes are inserted
 
-export function installAsynchronousAnnotator(node: Node = document, options: Options) {
-    let observer = new MutationObserver((mutations) => {
-        for (const mutation of mutations) {
-             for (const node of mutation.addedNodes) {
-                 annotateReactNodes(node, options);
-             }
-         }
-    })
-    annotateReactNodes(node, options);
-    observer.observe(node, { childList: true, subtree: true });
-    return () => observer.disconnect();
+export function installAsynchronousAnnotator(
+  node: Node = document,
+  options: Options
+) {
+  let observer = new MutationObserver(mutations => {
+    for (const mutation of mutations) {
+      for (const node of mutation.addedNodes) {
+        annotateReactNodes(node, options);
+      }
+    }
+  });
+  annotateReactNodes(node, options);
+  observer.observe(node, { childList: true, subtree: true });
+  return () => observer.disconnect();
 }
 
-export function installSynchronousAnnotator(node: Node = document, options: Options) {
-    const listener = (e: Event) => {
-        annotateReactNodes((e.target: any), options);
-    }
-    annotateReactNodes(node, options);
-    node.addEventListener("DOMNodeInserted", listener, false);
-    return () => node.removeEventListener("DOMNodeInserted", listener, false);
+export function installSynchronousAnnotator(
+  node: Node = document,
+  options: Options
+) {
+  const listener = (e: Event) => {
+    annotateReactNodes((e.target: any), options);
+  };
+  annotateReactNodes(node, options);
+  node.addEventListener("DOMNodeInserted", listener, false);
+  return () => node.removeEventListener("DOMNodeInserted", listener, false);
 }
 
 // HELPERS
 
 type ReactComponent = {
-    name: string,
-    displayName?: string,
+  name: string,
+  displayName?: string
 };
+type ReactType = string | ReactComponent;
 type ReactElement = {
-    type: ReactComponent,
-    _owner: ReactInternalInstance
+  type: ReactType,
+  _owner: ReactInternalInstance
 };
-type ReactInternalInstance = {
-    _currentElement: ReactElement
+type LegacyReactInternalInstance = {
+  _currentElement: ReactElement
 };
+type FiberNode = {
+  _debugOwner: FiberNode,
+  type: ReactType
+};
+type ReactInternalInstance = LegacyReactInternalInstance | FiberNode;
 
 export function getReactInternalInstance(node: ?Node): ?ReactInternalInstance {
-    if (node) {
-        for (const name in node) {
-            if (name.startsWith("__reactInternalInstance$")) {
-                // $FlowFixMe
-                return node[name];
-            }
-        }
+  if (node) {
+    for (const name in node) {
+      if (name.startsWith("__reactInternalInstance$")) {
+        // $FlowFixMe
+        return node[name];
+      }
     }
+  }
 }
 
 export function getReactOwner(node: ?Node): ?ReactInternalInstance {
-    let internalInstance = getReactInternalInstance(node);
-    if (internalInstance) {
-        return internalInstance._currentElement._owner;
-    }
+  let internalInstance = getReactInternalInstance(node);
+  if (internalInstance) {
+    return internalInstance._currentElement
+      ? // pre-Fiber
+        internalInstance._currentElement._owner
+      : // Fiber
+        internalInstance._debugOwner;
+  }
 }
 
 // works for both class and stateless function components
-export function getReactElementName(element: ReactElement): string {
-    return element.type.displayName || element.type.name;
+export function getReactType(instance: ReactInternalInstance): ?ReactType {
+  return instance._currentElement
+    ? // pre-Fiber
+      instance._currentElement.type
+    : // Fiber
+      instance.type;
+}
+
+export function getReactTypeName(instance: ReactInternalInstance): ?string {
+  const type = getReactType(instance);
+  return !type
+    ? null
+    : typeof type === "string"
+    ? type
+    : type.displayName || type.name;
 }
